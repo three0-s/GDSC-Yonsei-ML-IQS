acbb37dd57928725bb203b1973235d46be172e7e
Yewon Lim <ga06033@yonsei.ac.kr>
2023-11-06 02:44:35 +0900

    proxy

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,4 +3,5 @@
 python3 manage.py db init
 python3 manage.py db migrate --message init
 python3 manage.py db upgrade
-python3 manage.py run
+gunicorn --workers 3 --bind 0.0.0.0:8888 -m 007 "manage:run()"
+# python3 manage.py run
