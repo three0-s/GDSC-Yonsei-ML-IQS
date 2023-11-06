ff487ef3aca3699950aa8afac819d072521dcbc2
Yewon Lim <ga06033@yonsei.ac.kr>
2023-11-06 17:59:18 +0900

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(3.27.130.188)
+domains=(gdscpreview.rxby.dev)
 rsa_key_size=4096
 data_path="./certbot"
 email="ga06033@yonsei.ac.kr" # Adding a valid address is strongly recommended
